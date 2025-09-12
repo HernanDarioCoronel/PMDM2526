f2fc083718531919de18cfa4534b1a47d508bc5a
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-09-12 19:06:49 +0200

    Primeros pasos en Unity

---
+++ UD1/MyGame2D/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    KeyDownEvent keyPress;
+    void Start()
+    {
+        Debug.Log("Hola mundo!");
+    }
+
+    void Update()
+    {
+        Debug.Log("Hola cada frame");
+    }
+}
