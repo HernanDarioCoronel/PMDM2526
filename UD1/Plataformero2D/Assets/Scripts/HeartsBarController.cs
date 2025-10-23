4dc1dc87af997a729658aeb0f8b43be0a49f574d
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-10-23 19:09:47 +0200

    as

--- UD1/Plataformero2D/Assets/Scripts/HeartsBarController.cs
+++ UD1/Plataformero2D/Assets/Scripts/HeartsBarController.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HeartsBarController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int maxHearts = 5;
+    public int currentHearts = 3;
+
+    private VisualElement heartsContainer;
+
+    void OnEnable()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        heartsContainer = root.Q<VisualElement>("hearts-container");
+        UpdateHearts();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateHearts()
     {
-        
+        heartsContainer.Clear();
+
+        for (int i = 0; i < maxHearts; i++)
+        {
+            VisualElement heart = new VisualElement();
+            heart.AddToClassList("heart");
+
+            if (i < currentHearts)
+                heart.AddToClassList("full");
+            else
+                heart.AddToClassList("empty");
+
+            heartsContainer.Add(heart);
+        }
+    }
+
+    // Ejemplo: resta vida
+    public void TakeDamage(int amount)
+    {
+        currentHearts = Mathf.Max(currentHearts - amount, 0);
+        UpdateHearts();
+    }
+
+    // Ejemplo: cura vida
+    public void Heal(int amount)
+    {
+        currentHearts = Mathf.Min(currentHearts + amount, maxHearts);
+        UpdateHearts();
     }
 }
