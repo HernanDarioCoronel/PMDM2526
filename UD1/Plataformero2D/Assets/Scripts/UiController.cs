e6d378240a86bd3122de76360eebcd1db16aba3c
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-10-24 20:40:44 +0200

    missing music

--- UD1/Plataformero2D/Assets/Scripts/UiController.cs
+++ UD1/Plataformero2D/Assets/Scripts/UiController.cs
@@ -3,10 +3,28 @@ using UnityEngine.UIElements;
 
 public class UiController : MonoBehaviour
 {
+    public string nextSceneName;
     VisualElement root;
+    SceneController sceneController;
 
     void OnEnable()
     {
+        sceneController = GetComponent<SceneController>();
         root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button start = root.Q<Button>("start-button");
+        Button exit = root.Q<Button>("exit-button");
+
+        start.clicked += OnStartClicked;
+        exit.clicked += OnExitClicked;
+    }
+
+    void OnStartClicked()
+    {
+        sceneController.ChangeScene(nextSceneName);
+    }
+    void OnExitClicked()
+    {
+        sceneController.QuitGame();
     }
 }
