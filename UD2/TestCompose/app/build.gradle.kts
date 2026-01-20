b893b61a9ec618ac7af0a784dfb3daed02bdaa55
Hernan Coronel <dam.hernan.coronel@gmail.com>
2026-01-20 19:14:25 +0100

    8

--- UD2/TestCompose/app/build.gradle.kts
+++ UD2/TestCompose/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -16,7 +17,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -43,6 +43,11 @@ android {
 }
 
 dependencies {
+    //noinspection UseTomlInstead
+    implementation("com.google.code.gson:gson:2.13.2")
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- UD2/TestCompose/app/src/main/java/es/murallaromana/testcompose/MainActivity.kt
+++ UD2/TestCompose/app/src/main/java/es/murallaromana/testcompose/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -22,10 +24,16 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
 import es.murallaromana.testcompose.ui.theme.TestComposeTheme
 import es.murallaromana.testcompose.ui.theme.components.MiTopBar
 import es.murallaromana.testcompose.ui.theme.components.MisTextField
 import es.murallaromana.testcompose.ui.theme.layouts.MiColumna
+
+data object PrimeraPantalla
+data object SegundaPantalla
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +41,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TestComposeTheme {
+                val backStack = remember { mutableListOf<Any>(PrimeraPantalla) }
+
+                NavDisplay(
+                    backStack = backStack,
+                    entryProvider = { clave ->
+                        when (clave) {
+                            PrimeraPantalla -> NavEntry(PrimeraPantalla) { MiPrimeraPantalla() }
+                            SegundaPantalla -> NavEntry(SegundaPantalla) { MiSegundaPantalla() }
+                            else -> NavEntry(Unit) {}
+                        }
+                    }
+                )
+                /*
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     containerColor = Color.LightGray,
@@ -41,7 +62,7 @@ class MainActivity : ComponentActivity() {
                 ) { innerPadding ->
                     MisTextField(modifier = Modifier.padding(innerPadding))
                 }
-
+*/
             }
         }
     }
@@ -62,5 +83,17 @@ class MainActivity : ComponentActivity() {
             fontSize = 25.sp,
             textAlign = TextAlign.Center
         )
+    }
+
+    @Composable
+    fun MiPrimeraPantalla(modifier: Modifier = Modifier) {
+        Button(onClick = {}) { Text("Hola Primera Pantalla!") }
+
+    }
+
+    @Composable
+    fun MiSegundaPantalla(modifier: Modifier = Modifier) {
+        Button(onClick = {}) { Text("Hola Segunda Pantalla!") }
+
     }
 }
